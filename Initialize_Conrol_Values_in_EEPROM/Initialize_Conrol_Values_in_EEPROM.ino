2e0be1a0f2608a8828a4037a423bfcc276f6d937
Frank Bernett <fbernett@gmail.com>
2024-03-30 13:25:48 -0700

    Fixed looptimer saving to uint16_t

--- Initialize_Conrol_Values_in_EEPROM/Initialize_Conrol_Values_in_EEPROM.ino
+++ Initialize_Conrol_Values_in_EEPROM/Initialize_Conrol_Values_in_EEPROM.ino
@@ -12,7 +12,7 @@ int Boomdb = 0;
 float TrolleyG = .0268;
 float TrolleyI = 0;
 float TrolleyD = 0;
-float Trencoderperft = 7639.;
+float Trencoderperft = 15279.;
 int TrolleyClipOut = 2047; // in the model, out is a negative direction
 int TrolleyClipIn = -2047;
 int Trolleydb = 0;
@@ -28,7 +28,7 @@ int HoseReeldb = 0;
 float DragflowG = .0268;
 float DragflowI = 0;
 float DragflowD = 0;
-float Dfencoderperft = 7639.;
+float Dfencoderperft = 15279.;
 int DragflowClipUp = 2047;
 int DragflowClipDown = -2047;
 int Dragflowdb = 0;
@@ -39,7 +39,7 @@ float hoseGPosScale = .0245;
 int HoseGuideClipOut = 2047;
 int HoseGuideClipIn = -2047;
 int HoseGuidedb = 0;
-int looptimer = 2000; //This is a divider used to set loop interupt time
+uint16_t looptimer = 2000; //This is a divider used to set loop interupt time
 int movingOutVel = 75;  // This is about .5 ft/sec in encodercounts/sec
 int movingInVel = -10; // Used to chech if the trolley or dredge is moving in
 int hoseReelStall = -5; // checks to see if the hosereel has stalled pulling in the pipe
@@ -56,6 +56,7 @@ float Spare5;
 
 void setup() {
   Serial.begin(115200);
+
   pref.begin("ControlValues", false);
   pref.clear(); // clears the old values in ControlValues
   pref.putFloat("EEBoomG",BoomG);
@@ -102,7 +103,7 @@ void setup() {
   pref.putShort("EEDragfClipUp",DragflowClipUp);
   pref.putShort("EEDragfClipDn",DragflowClipDown);
   pref.putShort("EEDragflowdb",Dragflowdb);
-  pref.putShort("EELooptimer",looptimer);
+  pref.putUShort("EELooptimer",looptimer);
   pref.putShort("EEMovingOutVel",movingOutVel);
   pref.putShort("EEMovingInVel",movingInVel); 
   pref.putShort("EEHRStall",hoseReelStall);
@@ -117,9 +118,75 @@ void setup() {
  
   pref.end();
   Serial.println("Done Writing to EEPROM");
+
+  // Read last stored conrol loop values from eeprom
+  pref.begin("ControlValues", false);
+  BoomG = pref.getFloat("EEBoomG",0);
+  BoomI = pref.getFloat("EEBoomI",0);
+  BoomD = pref.getFloat("EEBoomD",0);
+  Bencoderperdeg = pref.getFloat("EEBEncodperdeg",0);
+  minBoomangle = pref.getFloat("EEMinBoomAngle",0);
+  maxBoomangle = pref.getFloat("EEMaxBoomAngle",0);
+  
+  TrolleyG = pref.getFloat("EETrolleyG",0);
+  TrolleyI = pref.getFloat("EETrolleyI",0);
+  TrolleyD = pref.getFloat("EETrolleyD",0);
+  Trencoderperft = pref.getFloat("EETEncodperft",0);
+  
+  DragflowG = pref.getFloat("EEDragflowG",0);
+  DragflowI = pref.getFloat("EEDragflowI",0);
+  DragflowD = pref.getFloat("EEDragflowD",0);
+  Dfencoderperft = pref.getFloat("EEDencodperft",0);
+
+  HoseReelG = pref.getFloat("EEHoseReelG",0);
+  HoseReelI = pref.getFloat("EEHoseReelI",0);
+  HoseReelD = pref.getFloat("EEHoseReel",0);
+  Hrencoderperft = pref.getFloat("EEHencodperft",0);
+  Hose2drag_ratio = pref.getFloat("EEHoseDragR",0);
+  Boomanglecomp = pref.getFloat("EEBoomAComp",0);
+ 
+  HoseGuideG = pref.getFloat("EEHoseGuideG",0);
+  HoseGuideI = pref.getFloat("EEHoseGuideI",0);
+  HoseGuideD = pref.getFloat("EEHoseGuideD",0);
+  hoseGPosScale =  pref.getFloat("EEHoseGPosScale",0);
+  
+  BoomClipRight = pref.getShort("EEBoomClipRight",0);
+  BoomClipLeft = pref.getShort("EEBoomClipLeft",0);
+  TrolleyClipOut = pref.getShort("EETrollClipOut",0);
+  TrolleyClipIn = pref.getShort("EETrollClipIn",0);
+  HoseReelClipOut = pref.getShort("EEHoseClipOut",0);
+  HoseReelClipIn = pref.getShort("EEHoseClipIn",0);
+  HoseGuideClipOut = pref.getShort("EEHoseGClipOut",0);
+  HoseGuideClipIn = pref.getShort("EEHoseGClipIn",0);
+  DragflowClipUp = pref.getShort("EEDragfClipUp",0);
+  DragflowClipDown = pref.getShort("EEDragfClipDn",0);
+  
+  Boomdb = pref.getShort("EEBoomdb",0);  
+  Trolleydb = pref.getShort("EETrolleydb",0); 
+  HoseReeldb = pref.getShort("EEHoseRealdb",0);
+  HoseGuidedb = pref.getShort("EEHoseGuidedb",0); 
+  Dragflowdb = pref.getShort("EEDragflowdb",0);
+  looptimer = pref.getUShort("EELooptimer",0);
+  movingOutVel = pref.getShort("EEMovingOutVel",0);
+  movingInVel = pref.getShort("EEMovingInVel",0); 
+  hoseReelStall = pref.getShort("EEHRStall",0);
+  MaxDdepthchange = pref.getFloat("EEDrDepthChange",0);
+  MaxBAngchange = pref.getFloat("EEBAngChange",0);
+  MaxTrollchange = pref.getFloat("EETrolChange",0);
+  Spare1 = pref.getFloat("EESpare1",0);
+  Spare2 = pref.getFloat("EESpare2",0);
+  Spare3 = pref.getFloat("EESpare3",0);
+  Spare4 = pref.getFloat("EESpare4",0);
+  Spare5 = pref.getFloat("EESpare5",0);
+  pref.end();
+
+
+  Serial.print("Current looptimer value ="); Serial.println(looptimer);
+  Serial.print("Current HoseGuideClipOut value ="); Serial.println(HoseGuideClipOut);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  delay(10);
 
 }
